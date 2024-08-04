271a13d308c12ac4ade669eba2a061c76bfb0c9e
RONALDMASLOG2024-2025 <rmaslog_230000000654@uic.edu.ph>
2024-08-04 19:23:01 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+var display = document.getElementById("display");
+
+function btn1() {
+  display.innerText = display.innerText + "1";
+}
+
+function btn2() {
+  display.innerHTML = display.innerText + "2";
+}
+
+function btn3() {
+  display.innerHTML = display.innerText + "3";
+}
+function btn4() {
+  display.innerHTML = display.innerText + "4";
+}
+
+function btn5() {
+  display.innerHTML = display.innerText + "5";
+}
+
+function btn6() {
+  display.innerHTML = display.innerText + "6";
+}
+
+function btn7() {
+  display.innerHTML = display.innerText + "7";
+}
+
+function btn8() {
+  display.innerHTML = display.innerText + "8";
+}
+
+function btn9() {
+  display.innerHTML = display.innerText + "9";
+}
+
+
+function btn0() {
+    display.innerHTML = display.innerText + "0";
+  }
